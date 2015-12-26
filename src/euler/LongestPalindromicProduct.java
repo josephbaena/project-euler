4ef35dadcadd801b072b439cd084166e14ef912a
Joseph Baena <jbaena@stanford.edu>
2015-12-26 13:14:43 -0800

    faster implementation for problem 4

--- src/euler/LongestPalindromicProduct.java
+++ src/euler/LongestPalindromicProduct.java
@@ -8,10 +8,15 @@ package euler;
 public class LongestPalindromicProduct {
 
     public static void main(String[] args) {
+        faster();
+//        slower();
+    }
+
+    private static void slower() {
         int largest = 0;
         int largest_i = 0, largest_j = 0;
         for (int i = 999; i > 99; i--) {
-            for (int j = i; j > 99; j--) {
+            for (int j = 999; j > i; j--) {
                 int product = i * j;
                 if ((product > largest) && (product == reverse(product))) {
                     largest = product;
@@ -31,5 +36,34 @@ public class LongestPalindromicProduct {
             n /= 10;
         }
         return reversed;
+    }
+
+    private static void faster() {
+        int largest = 0, largest_i = 0, largest_j = 0;
+        for (int i = 999; i > 99; i--) {
+            int j, divisor;
+            if (i % 11 == 0) {
+                j = 999;
+                divisor = 1;
+            } else {
+                j = 990;
+                divisor = 11;
+            }
+
+            while (j >= i) {
+                int product = i * j;
+                if (product <= largest) break;
+
+                if (product == reverse(product)) {
+                    largest = product;
+                    largest_i = i;
+                    largest_j = j;
+                }
+
+                j -= divisor;
+            }
+        }
+
+        System.out.println("Largest palindrome: " + largest_i + " * " + largest_j + " = "  + largest);
     }
 }
